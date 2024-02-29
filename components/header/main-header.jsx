6fb37b5ce4619c28922c8021acd498e1463b323f
Zeeshan Safdar <zeeshan7826@gmail.com>
2024-02-29 11:17:38 +0500

    optimize image and add background svg component

--- components/header/main-header.jsx
+++ components/header/main-header.jsx
@@ -1,27 +1,32 @@
 import Link from "next/link";
+import Image from "next/image";
 import React from "react";
 
+import HeaderBackground from "./main-header-background";
 import logoImg from "@/assets/logo.png";
 import styles from "./main-header.module.css";
 
 function Header() {
   return (
-    <header className={styles.header}>
-      <Link href={"/"} className={styles.logo}>
-        <img src={logoImg.src} alt="Food Logo" /> NextLevel Food
-      </Link>
+    <>
+      <HeaderBackground />
+      <header className={styles.header}>
+        <Link href={"/"} className={styles.logo}>
+          <Image src={logoImg} alt="Food Logo" priority /> NextLevel Food
+        </Link>
 
-      <nav className={styles.nav}>
-        <ul>
-          <li>
-            <Link href={"/meals"}>Browse Meal</Link>
-          </li>
-          <li>
-            <Link href={"/community"}>Community</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+        <nav className={styles.nav}>
+          <ul>
+            <li>
+              <Link href={"/meals"}>Browse Meal</Link>
+            </li>
+            <li>
+              <Link href={"/community"}>Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
 
