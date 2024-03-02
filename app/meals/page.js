2a249e4452220082ee3b14f02a0e7c5ed0851f03
Zeeshan Safdar <zeeshan7826@gmail.com>
2024-03-02 15:54:57 +0500

    add empty meals grid

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,23 @@
-import React from "react";
+import Link from "next/link";
 
-function Meals() {
-  return <div>Meals</div>;
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid.jsx";
+
+export default function MealsPage() {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>Choose your favorite recipe and cook it yourself. It is easy and fun!</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 }
-
-export default Meals;
