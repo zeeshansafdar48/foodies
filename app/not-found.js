70fbdc3df639f6b3cdd53cd2537459c61ee499da
Zeeshan Safdar <zeeshan7826@gmail.com>
2024-03-03 11:50:34 +0500

    add error.js and not-found.js files

---
+++ app/not-found.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Not Found</h1>
+      <p>Sorry! We could not found the requested page or resource</p>
+    </main>
+  );
+}
+
+export default NotFound;
